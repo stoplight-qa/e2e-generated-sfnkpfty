a3d16491d31f460bc613a39d94e96d58f639a922
qa-giqmo-2-reex@example.com <qa-giqmo-2-reex@example.com>
2023-04-18 15:14:25 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16054.stoplight-dev.com/api/v1/projects/cHJqOjUzNA/spectral.js?branch_slug=main&token=98dcaca8-9519-4355-ba0e-e23d7dec048f";
+export default { extends: ruleset };
